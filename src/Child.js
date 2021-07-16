96a2877b846520fdd70b1c8b5b281d047d67d0cb
bilalfareed83 <bilal_fareed@hotmail.com>
2021-07-16 17:31:45 +0500

    update child.js

--- src/Child.js
+++ src/Child.js
@@ -4,15 +4,38 @@ function Child() {
   const { transations, addTransation } = useContext(TransationContext);
   const [newAmount, setAmount] = useState(0);
   const [newDes, setDes] = useState("");
+  const [editItne, setEditItem] = useState(true);
+  const [editItemId, setEditItemId] = useState(null);
 
   const handleAddition = (event) => {
     event.preventDefault();
     if (!newAmount || !newDes) return;
+
+    if (!editItne) {
+      transations.map((item) => {
+        if (item.id === editItemId) {
+          return item;
+        }
+        addTransation({
+          amount: newAmount,
+          des: newDes,
+        });
+      });
+      return setAmount(0), setDes(""), setEditItem(false);
+    }
     addTransation({
       amount: newAmount,
       des: newDes,
     });
-    return setAmount(0), setDes("");
+    return setAmount(0), setDes(""), setEditItem(true);
+  };
+
+  const editFun = (obj) => {
+    console.log(obj, "edit func");
+    setEditItem(false);
+    setAmount(obj.amount);
+    setDes(obj.des);
+    setEditItemId(obj.id);
   };
 
   const amount = transations.map((item) => Number(item.amount));
@@ -26,7 +49,7 @@ function Child() {
 
   return (
     <div className="container">
-      {console.log(incomeItem)}
+      {/* {console.log(incomeItem)} */}
       <h2 className="text-center">Expenses tracker</h2>
       <h2>
         Your Balance <br /> {total}
@@ -43,12 +66,13 @@ function Child() {
       <h2>History</h2>
       <hr />
       <ul className="trasation-list">
-        {transations.map((obj, index) => {
-          const { des, amount } = obj;
+        {transations.map((obj) => {
+          const { des, amount, id } = obj;
           return (
-            <li key={index}>
+            <li key={id}>
               <span>{des}</span>
               <span>{amount}</span>
+              <span onClick={() => editFun(obj)}>edit</span>
             </li>
           );
         })}
@@ -76,7 +100,11 @@ function Child() {
           />
         </label>
         <br />
-        <input type="submit" value="Add Transation" />
+        {editItne ? (
+          <input type="submit" value="Add Transation" />
+        ) : (
+          <input type="submit" value="Edit Transation" />
+        )}
       </form>
     </div>
   );
